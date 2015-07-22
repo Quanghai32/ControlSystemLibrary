b019da55defd4d6c5cdb102fbc5231f68d26d9aa
Nguyen Minh Tien <apr-adm32@cg.canon-vn.com.vn>
2015-07-22 15:21:32 +0700

    Change assembly version information

--- ControlSystemLibrary/My Project/AssemblyInfo.vb
+++ ControlSystemLibrary/My Project/AssemblyInfo.vb
@@ -9,8 +9,8 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("ControlSystemLibrary")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyDescription("Library for AGV control system")> 
+<Assembly: AssemblyCompany("Canon Viet Nam")> 
 <Assembly: AssemblyProduct("ControlSystemLibrary")> 
 <Assembly: AssemblyCopyright("Copyright ©  2014")> 
 <Assembly: AssemblyTrademark("")> 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("2.1.0.0")> 
+<Assembly: AssemblyFileVersion("2.1.0.0")> 
